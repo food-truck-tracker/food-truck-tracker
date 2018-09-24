c1def0abdadba6a37fbaf7fed342866aee24f6a4
muedie <lasafalus@gmail.com>
2018-09-23 19:47:21 -0500

    added textinput

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,22 +1,68 @@
-import React from 'react';
-import {
-    Text,
-    View,
-    Button
-} from 'react-native';
+import React from "react";
+import { Text, View, Button, TextInput, StyleSheet } from "react-native";
 
 export default class LoginForm extends React.Component {
-    render() {
-        return (
-            <View>
-                <Text>Login</Text>
-                <Button 
-                    title="Go back"
-                    onPress={() => {
-                        this.props.changeView()
-                    }}
-                />
-            </View>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Login</Text>
+
+        <TextInput
+          autoCorrect={false}
+          placeholder="Enter your email..."
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
+        />
+
+        <TextInput
+          autoCorrect={false}
+          placeholder="Enter your password..."
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
+          secureTextEntry={true}
+        />
+
+        <Button
+          title="Login"
+          onPress={() => console.log("TODO: firebase auth")}
+        />
+
+        <View
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: StyleSheet.hairlineWidth
+          }}
+        />
+
+        <Button
+          title="Login With Google"
+          onPress={() => {
+            // this.props.changeView()
+          }}
+        />
+
+        <Button
+          title="Login With Facebook"
+          onPress={() => {
+            // this.props.changeView()
+          }}
+        />
+
+        <Button
+          title="Go back"
+          onPress={() => {
+            this.props.changeView();
+          }}
+        />
+      </View>
+    );
+  }
 }
