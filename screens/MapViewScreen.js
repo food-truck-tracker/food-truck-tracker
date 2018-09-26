a2e9b2457acd406f2e22816f17b9387a4cb5b9f7
muedie <lasafalus@gmail.com>
2018-09-26 13:05:15 -0500

    maps view component

--- screens/MapViewScreen.js
+++ screens/MapViewScreen.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import {
-    Text,
-    View,
-    StyleSheet,
-} from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 export default class MapViewScreen extends React.Component {
-    render() {
-        return (
-            <View>
-                <Text style={styles.header}>Map View</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    header: {
-      fontSize: 32,
-      fontWeight: "bold",
-      padding: 10,
-      margin: 10
-    }
-  });
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
