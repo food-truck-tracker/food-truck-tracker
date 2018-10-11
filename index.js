e974b370a047ea3ba9973ca18d830bfe0bb13dfa
muedie <lasafalus@gmail.com>
2018-10-11 18:00:38 -0500

    app connection with redux store

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-import { AppRegistry } from "react-native";
+import React from "react";
+import { AppRegistry, Text } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
+import configureStore from "./src/config/store";
 import App from "./src/App";
 import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+const { store, persistor } = configureStore();
+
+const ConnectedApp = () => (
+  <Provider store={store}>
+    <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ConnectedApp);
