6f7ccb5f26f3055caa6735f0910fb69c91b977ba
muedie <lasafalus@gmail.com>
2018-11-18 16:00:58 -0600

    review ducks done!

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import auth from "./auth";
 import truck from "./truck";
 import location from "./location";
+import review from "./review";
 
 export default combineReducers({
   auth,
   truck,
   location,
+  review,
 });
